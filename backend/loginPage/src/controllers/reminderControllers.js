298d6bd5ddc3ae4f3ac9c369274d3240df7bd882
Sachila Dissanayake <sathmika7@gmail.com>
2025-02-26 22:56:28 +0530

    Update reminderControllers.js

--- backend/loginPage/src/controllers/reminderControllers.js
+++ backend/loginPage/src/controllers/reminderControllers.js
@@ -18,3 +18,26 @@ const getReminderSettings = async (req, res) => {
         res.status(500).json({ message: 'Server Error'});
     }
 };
+
+// @desc    Update reminder settings
+// @route   PUT /api/reminders
+// @access  Private
+
+const updateReminderSettings = async (req, res) => {
+    try{
+        const { postureReminder, postureInterval, waterReminder, waterInterval, standReminder, standInterval } = req.body;
+
+        const settings = await ReminderSettings.findOne(
+            {user : req.user._id},
+            { postureReminder, hydrationReminder, movementReminder },
+            { new : true, upsert : true}
+        );
+
+            res.json(settings);
+    } catch(error){
+        console.error(error);
+        res.status(500).json({ message: 'Server Error'});
+    }
+};
+
+module.exports = { getReminderSettings, updateReminderSettings };
