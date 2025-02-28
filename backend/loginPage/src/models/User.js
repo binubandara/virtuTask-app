0378b49ee9e00c2e24f032a25dc199eeaf60108e
Sachila Dissanayake <sathmika7@gmail.com>
2025-02-28 13:40:02 +0530

    Update User.js

--- backend/loginPage/src/models/User.js
+++ backend/loginPage/src/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+// User schema definition
 const userSchema = new mongoose.Schema({
     username : {
         type : String,
