b8d947bd305f3dada4c4ae2b880f46f3d1ef9594
Sachila Dissanayake <sathmika7@gmail.com>
2025-02-28 13:33:56 +0530

    Update reminderRoutes.js

--- backend/loginPage/src/routes/reminderRoutes.js
+++ backend/loginPage/src/routes/reminderRoutes.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const{protect} = require('../middleware/authMiddleware');
+const{getReminder, updateReminder} = require('../controllers/reminderControllers');
 
-const {
-    getReminderSettings,
-    updateReminderSettings
-} = require('../controllers/reminderControllers');
-
-router.use(protect);
-
-router.route('/').get(getReminderSettings).put(updateReminderSettings);
+// Reminder routes
+router.route('/settings')
+    .get(protect, getReminder)
+    .put(protect, updateReminder);
 
 module.exports = router;
