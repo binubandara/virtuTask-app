e6e09f25bcbfd58e5071f02533030a9705c08804
Sachila Dissanayake <sathmika7@gmail.com>
2025-02-28 13:40:00 +0530

    Update ReminderSettings.js

--- backend/loginPage/src/models/ReminderSettings.js
+++ backend/loginPage/src/models/ReminderSettings.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// Reminder settings schema definition
 const reminderSettingSchema = new mongoose.Schema({
     user : {
         type : mongoose.Schema.Types.ObjectId,
