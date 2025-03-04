e4b046c6149bc144fed5a09c45834c8a1245d09e
Neluni Fernando <neluni.sf@gmail.com>
2025-03-04 13:01:15 +0530

    Updated global Time Sync to render ClockDashboard in the pane

---
+++ frontend/src/components/globalTime/ClockDashboard.jsx
@@ -0,0 +1,13 @@
+// components/pane/Dashboard.jsx
+import React from 'react';
+
+const ClockDashboard = () => {
+  return (
+    <div>
+      <h1> -- new page try out yay</h1>
+      <p>lets test it out </p>
+    </div>
+  );
+};
+
+export default ClockDashboard;
--- frontend/src/components/pane/PanePage.jsx
+++ frontend/src/components/pane/PanePage.jsx
@@ -4,9 +4,9 @@ import { Routes, Route } from 'react-router-dom';
 import Logo from './Logo';
 import MenuList from './MenuList';
 import Profile from './Profile';
-import Settings from '../settingsPage/Settings'; // Import Settings Page
-import Dashboard from './Dashboard'; // Import other components as needed
-
+import Settings from '../settingsPage/Settings';
+import Dashboard from './Dashboard'; 
+import ClockDashboard from '../globalTime/ClockDashboard';  
 import GlobalSync from './GlobalSync';
 import ProfilePage from '../userProfile/ProfilePage';
 
@@ -17,17 +17,16 @@ const PanePage = () => {
     <Layout style={{ minHeight: '100vh' }}>
       <Sider theme="light" width={250}>
         <Logo />
-        <MenuList />  {/* Pass function properly */}
-        <Profile />  {/* Profile will always be visible in the sidebar */}
+        <MenuList />  
+        <Profile />  
       </Sider>
       <Content style={{ padding: '24px' }}>
         <Routes>
-          <Route path="/" element={<Dashboard />} /> {/* Default route for /pane */}
+          <Route path="/" element={<Dashboard />} /> 
           <Route path="dashboard" element={<Dashboard />} />
-          <Route path="global-sync" element={<GlobalSync />} />
+          <Route path="global-sync" element={<ClockDashboard />} />
           <Route path="settings" element={<Settings />} />
           <Route path="profile" element={<ProfilePage/>} />
-          {/* Add other routes here */}
         </Routes>
       </Content>
     </Layout>
