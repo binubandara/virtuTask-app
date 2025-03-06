030d2b2fa901285d9a0f949b0e43019c93a8fb76
Thishakya Kumarathung <tskumarathunga@gmail.com>
2025-03-06 23:08:53 +0530

    removed unnecessary files re-write json files root directory

--- frontend/src/component/Button.jsx
+++ frontend/src/component/Button.jsx
@@ -1,10 +1,9 @@
 const Button = ({ title, activeClass, _callback }) => {
-    return (
-      <button className={activeClass} onClick={_callback}>
-        {title}
-      </button>
-    );
-  };
-  
-  export default Button;
-  
+  return (
+    <button className={activeClass} onClick={_callback}>
+      {title}
+    </button>
+  );
+};
+
+export default Button;
